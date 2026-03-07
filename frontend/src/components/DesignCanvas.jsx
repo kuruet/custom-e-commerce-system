99f3490e60c3795212390b5cb105b1ab005dbe4d
kuruet <bpratik728@gmail.com>
2026-03-07 15:29:25 +0530

    Phase 3 Step 3: Fabric canvas engine working

--- frontend/src/components/DesignCanvas.jsx
+++ frontend/src/components/DesignCanvas.jsx
@@ -1,28 +1,31 @@
 import { useEffect, useRef } from "react";
-import { Canvas } from "fabric";
+import { Canvas, Textbox } from "fabric";
 
 export default function DesignCanvas() {
   const canvasRef = useRef(null);
-  const fabricRef = useRef(null);
 
   useEffect(() => {
     const canvas = new Canvas(canvasRef.current, {
-      width: 400,
-      height: 400,
+      width: 500,
+      height: 500,
       backgroundColor: "#ffffff",
-      selection: true,
     });
 
-    fabricRef.current = canvas;
+    // TEMP TEST OBJECT
+    const text = new Textbox("Edit me", {
+      left: 150,
+      top: 200,
+      fontSize: 30,
+      fill: "black"
+    });
 
-    return () => {
-      canvas.dispose();
-    };
+    canvas.add(text);
+
   }, []);
 
   return (
-    <div className="border rounded-lg p-4 bg-gray-50">
-      <canvas ref={canvasRef} />
+    <div className="border-2 border-gray-300 rounded-lg p-4 bg-gray-100 flex justify-center items-center">
+      <canvas ref={canvasRef}/>
     </div>
   );
 }
