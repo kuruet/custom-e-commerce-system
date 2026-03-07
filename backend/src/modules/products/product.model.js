c32a8b188085be2e0dc2dfbca7de7e277fc956ef
kuruet <bpratik728@gmail.com>
2026-03-07 14:02:24 +0530

    Phase 2 Step 2: Product schema implemented

--- backend/src/modules/products/product.model.js
+++ backend/src/modules/products/product.model.js
@@ -1,1 +1,41 @@
-export {};
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      min: 0
+    },
+
+    description: {
+      type: String,
+      required: true
+    },
+
+    image: {
+      type: String,
+      required: true
+    },
+
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+      min: 0
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
