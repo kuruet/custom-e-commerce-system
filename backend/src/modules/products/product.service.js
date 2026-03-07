3ae80ef96e5083dba4ee418af6f8a612e302d9b7
kuruet <bpratik728@gmail.com>
2026-03-07 14:03:36 +0530

    Phase 2 Step 3: Product service layer implemented

--- backend/src/modules/products/product.service.js
+++ backend/src/modules/products/product.service.js
@@ -1,1 +1,41 @@
-export {};
+import Product from "./product.model.js";
+
+
+// Create new product
+export const createProduct = async (productData) => {
+  const product = await Product.create(productData);
+  return product;
+};
+
+
+// Get all products
+export const getAllProducts = async () => {
+  const products = await Product.find().sort({ createdAt: -1 });
+  return products;
+};
+
+
+// Get single product by ID
+export const getProductById = async (productId) => {
+  const product = await Product.findById(productId);
+  return product;
+};
+
+
+// Update product
+export const updateProduct = async (productId, updateData) => {
+  const updatedProduct = await Product.findByIdAndUpdate(
+    productId,
+    updateData,
+    { new: true }
+  );
+
+  return updatedProduct;
+};
+
+
+// Delete product
+export const deleteProduct = async (productId) => {
+  const deletedProduct = await Product.findByIdAndDelete(productId);
+  return deletedProduct;
+};
